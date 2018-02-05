f6b0fa5de9ea0f3596a6ff6c72d74fedf25a9e60
halfstopper <weiyang_30@live.com>
2018-02-05 13:40:55 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-link :to="{ name: 'Hello' }">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/coins/ethereum">Ethereum</router-link>
+    <router-link to="/coins/bitcoin">Bitcoin</router-link>
+    <router-view></router-view>
+  </div>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import About from '@/components/About'
+import Coins from '@/components/Coins'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/coins/:id',
+      name: 'Coins',
+      component: Coins
+    }
+      
+  ]
+})
+
+
+/*
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
+})
+*/
+
